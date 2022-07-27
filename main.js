c3db478b4b448b479dd709f010f8b67258724874
Seigi Gim <seigi.fgim@gmail.com>
2022-07-26 20:07:49 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const inputCard = document.getElementById("inputCard");
+const inputDate = document.getElementById("inputDate");
+const inputCVV = document.getElementById("inputCVV");
+
+const maskNumber = "####-####-####-####";
+const maskDate = "##/##";
+const maskCVV = "###";
+
+let current = "";
+let cardNumber = [];
+let dateNumber = [];
+let cvvNumber = [];
+
+//INPUTCARD----------------------------------------
+inputCard.addEventListener("keydown", (e) => {
+  if (e.key === "Tab") {
+    return;
+  }
+
+  e.preventDefault();
+  handleInput(maskNumber, e.key, cardNumber);
+  inputCard.value = cardNumber.join("");
+});
+//-------------------------------------------------
+
+//INPUTDATE----------------------------------------
+inputDate.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+        return;
+    }
+
+    e.preventDefault();
+    handleInput(maskDate, e.key, dateNumber)
+    inputDate.value = dateNumber.join("");
+})
+//-------------------------------------------------
+
+//INPUTCVV-----------------------------------------
+inputCVV.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+      return;
+    }
+  
+    e.preventDefault();
+    handleInput(maskCVV, e.key, cvvNumber);
+    inputCVV.value = cvvNumber.join("");
+  });
+//------------------------------------------------
+
+function handleInput(mask, key, arr) {
+  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  if (key === "Backspace" && arr.length > 0) {
+    arr.pop();
+    return;
+  }
+  if (numbers.includes(key) && arr.length + 1 <= mask.length) {
+    if (mask[arr.length] === "-" || mask[arr.length] === "/") {
+      arr.push(mask[arr.length], key);
+    } else {
+      arr.push(key);
+    }
+  }
+}
